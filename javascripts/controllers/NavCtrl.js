8e3cfcda092b47f48113db0db7650945bac2f808
gib <gibbowden@gmail.com>
2017-11-30 15:54:03 -0600

    initial setup done

---
+++ javascripts/controllers/NavCtrl.js
@@ -0,0 +1,17 @@
+"use strict"; 
+
+app.controller("NavCtrl", function($location, $rootScope, $scope, $window, AuthService){
+
+    $scope.logoutUser = () => {
+        delete $rootScope.uid; 
+        $window.localStorage.clear(); 
+        AuthService.logout(); 
+        $location.path('/login'); 
+    };
+    
+
+    $rootScope.searchTextNav = {
+        text: ""
+    };  
+
+});
