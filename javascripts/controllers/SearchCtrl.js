cfc59cbe60fc432c5b1be1397c3b57a2ec14f933
gib <gibbowden@gmail.com>
2017-12-07 23:32:52 -0600

    cleaned up commented out code

--- javascripts/controllers/SearchCtrl.js
+++ javascripts/controllers/SearchCtrl.js
@@ -2,25 +2,6 @@
 
 app.controller("SearchCtrl", function($location, $rootScope, $scope, SongKickService){
 
-//takes the artist search box value via kepress (enter) event
-//calls the getArtist request 
-//returns the best match (first result)
-
-    // $scope.enterPushArtistSearch = (e) => {
-    //     if (e.keyCode === 13) {
-    //         SongKickService.getArtists(e.target.value).then((results) => {
-    //             let bestResult = results[0]
-    //             console.log(bestResult);
-    //             getArtistConcerts(bestResult.id, `2017-12-20`, `2017-12-25`, '1'); 
-    //         }).catch((err) => {
-    //             console.log(err); 
-    //         });
-    //     };
-    // }; 
-
-//takes a artistId (required), start date (optional), end date (optional), and page number (optional)
-//calls the getConcertsByMetroId function from the service
-//sets the artist scope variable
 
     const getArtistConcerts = (artist, city, startDate, endDate, pageNumber) => {
         SongKickService.getArtists(artist).then((results) => {
@@ -37,6 +18,7 @@ app.controller("SearchCtrl", function($location, $rootScope, $scope, SongKickSer
             console.log(err); 
         });
     };
+    
 
     const filterArtistConcertsByCity = (cityQuery, concertArr) => {
         let regex = RegExp(`(${cityQuery})`, 'ixg');
@@ -47,9 +29,6 @@ app.controller("SearchCtrl", function($location, $rootScope, $scope, SongKickSer
     }; 
 
 
- //takes a metroId (required), start date (optional), end date (optional), and page number (optional)
- //calls the getConcertsByMetroId function from the service
- //sets the metro area concerts scope variable
     const getUsMetroConcerts = (cityQuery, startDate, endDate, pageNumber) => {
         SongKickService.getBestMetroResult(cityQuery, 'US').then((result) => {
             return SongKickService.getConcertsByMetroId(result.metroArea.id, startDate, endDate, pageNumber)
