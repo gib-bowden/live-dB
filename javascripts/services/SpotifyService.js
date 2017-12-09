619bd8069067588d4e9c737001aeb7cabea59a03
gib <gibbowden@gmail.com>
2017-12-09 13:04:09 -0600

    added a bunch of spotify auth stuff

--- javascripts/services/SpotifyService.js
+++ javascripts/services/SpotifyService.js
@@ -1,31 +1,46 @@
-"use strict"; 
+'use strict';
 
-app.service("SpotifyService", function($q, $http){
+app.service("SpotifyService", function($http, $window, $rootScope, $location, LoginService){
+    let spotifyWindow;
 
-    const authenticate = () => {
-        return $q((resolve, reject) => {
-            $http.get('https://mighty-shelf-28254.herokuapp.com/login').then((results) => {
-                console.log("results from /login", results); 
-                resolve(); 
-            }).then((data) => {
-                console.log("results from /authorize", data);
-            }).catch((err) => {
-                console.log(err);
-            });
-        });
-    }; 
-    
-    const sendAuthenticationData = (authData) => {
-        return $q((resolve, reject) => {
-            $http.get(`https://accounts.spotify.com/authorize?${authData}`).then((results) => {
-                resolve(results);
-            }).catch((err) => {
-                console.log(err); 
-            });
+    const getCurrentSpotifyId = () => {
+        return localStorage.getItem('spotifyUserId');
+    }
+
+    const authenticateUser = () => {
+        localStorage.removeItem('spotifyUserId');
+        $http.get(`https://mighty-shelf-28254.herokuapp.com/login`)
+        .then(( results ) => {
+            spotifyWindow = $window.open(results.data, 'spotify', "menubar=1,resizable=1,width=350,height=250");
+        }).catch((err) => {
+            console.log('error in authenticeUser:', err);
         });
     };
 
-    return {authenticate}; 
-});
+    const closeSpotifyWindow = () => {
+        spotifyWindow.close();
+    };
 
- 
+    const getSpotifyPlaylists = () => {
+        return $http.get(`https://mighty-shelf-28254.herokuapp.com/userPlaylist?user=${getCurrentSpotifyId()}`); 
+    };
+
+    firebase.database().ref('userDetails/').on("child_changed" || "child_added", function(snapshot) {
+        if (snapshot.val() !== null && !getCurrentSpotifyId()) {
+            closeSpotifyWindow();
+            const spotifyUserId = snapshot.key;
+            console.log(spotifyUserId);
+            const userData = snapshot.val();
+            localStorage.setItem('spotifyUserId', spotifyUserId);
+            LoginService.addFirebaseUser(userData.email, userData.spotifyId).then((uid) => {
+                console.log(uid);
+                firebase.database().ref('userDetails/' + spotifyUserId).update({'uid': uid});
+            })
+            $rootScope.$apply(() => {$location.path('/main')});         
+        }
+      }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
+    
+    return { authenticateUser, getSpotifyPlaylists, getCurrentSpotifyId };
+});
